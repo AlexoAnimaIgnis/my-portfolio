8a531d84ee613a1ea97f98f82e8a327ffcf39c4f
unknown <ballester.alexander@gmail.com>
2020-07-10 23:33:06 +0800

    modified footer description

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,7 +10,7 @@ export default class Footer extends React.Component {
               <div className="f_title">
                 <h3>About Me</h3>
               </div>
-              <p>For freelance work please don't hesitate to contact me through my social media or email. I also accept art commissions.</p>
+              <p>For freelance work please do not hesitate to contact me through my social media or email. I also accept art commissions.</p>
               <p>{/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
                 Copyright © All rights reserved | This template is made with <i className="fa fa-heart-o" aria-hidden="true" /> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
                 {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}</p>
